c7255f7801e53bb2a85d219437539f80278c5e87
Virgil Griffith <i@virgil.gr>
2015-08-13 07:45:53 -0400

    got WA working

--- hello.py
+++ hello.py
@@ -7,11 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	app_id = 'E7W676-QR2UE4PUR4'
-	client = wolframalpha.Client('app_id')
+	client = wolframalpha.Client(app_id)
 	res = client.query('temperature in Washington, DC on October 3, 2012')
 
-	stuff = next(res.results).text
+	#res = client.query('1337')
 
-    return stuff
+	stuff = []
+	for r in res.results:
+		stuff.append( r.text )
+	return '<br>'.join(stuff)
 
 
