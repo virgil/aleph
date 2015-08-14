497c0cec29eaea44c6cff856b02b43b586396972
virgil <i@virgil.gr>
2015-08-14 16:43:52 +0800

    added /junk

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-import wolframalpha
-import os
-from flask import Flask
+from flask import Flask, redirect, render_template
+import os, wolframalpha, re
+
+notnumber_pattern = re.compile(r'[^0-9]')
+
 
 app = Flask(__name__)
 
@@ -12,19 +14,14 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-@app.route('/<int:num>')
-def page(num):
-    # show the post with the given id, the id is an integer
-    if num < 0:
-    	return "Naturals numbers only!"
-
-    return 'This is the page for the glorious natural number: %d' % num
-
-
 @app.route('/why')
 def why_page():
 	return "This page exists because people should stop talking about how big the deep web / dark web is."
 
+
+@app.route('/random')
+def why_page():
+	return "This page selects a random /number page from the cache."
 
 @app.route('/wolfram')
 def wolfram():
@@ -39,18 +36,23 @@ def wolfram():
 	return '<br>'.join(stuff)
 
 
+@app.route('/<int:num>')
+def page(num):
+    # show the post with the given id, the id is an integer
+    if num < 0:
+    	return "Naturals numbers only!"
 
-'''
-@app.route('/')
-def hello():
-
-
-	#res = client.query('1337')
-
-	stuff = []
+    return 'This is the page for the glorious natural number: %d' % num
 
 
-	the_path = "my path is=%s " % request.path
-	return the_path
+## This is for doing redirection for unknown paths
+@app.route('/<path:junk>')
+def redirect_url(junk):
+	global notnumber_pattern
+	junk = re.sub( notnumber_pattern, '', junk )
+	
+	# if junk exists, make it an int (to remove initial 0s, else make it a zero)
+	junk = int(junk) if junk else ''
+	
+	redirect('/%s' % junk, 302)
 
-'''
